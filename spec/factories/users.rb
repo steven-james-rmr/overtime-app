8c43ff4c7efdd2eceea9072529d35b4369a7b973
steven-james-rmr <rockymtnrednecks@gmail.com>
2017-08-29 17:22:14 -0600

    Implemented edit and update functionality

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,19 @@
 FactoryGirl.define do
-  factory :user do 
-    first_name "Jon"
-    last_name "Snow"
+  sequence :email do |n|
+    "test#{n}@example.com"
+  end
+
+  factory :user do
+    first_name 'Jon'
+    last_name 'Snow'
     email "test@test.com"
     password "asdfasdf"
     password_confirmation "asdfasdf"
   end
 
-  factory :admin_user, class: "AdminUser" do 
-    first_name "Admin"
-    last_name "User"
+  factory :admin_user, class: "AdminUser" do
+    first_name 'Admin'
+    last_name 'User'
     email "admin@user.com"
     password "asdfasdf"
     password_confirmation "asdfasdf"
