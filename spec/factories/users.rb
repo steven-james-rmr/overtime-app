b6110e943ec6d8b0aac6e4049d7fba90baeb37ef
steven-james-rmr <rockymtnrednecks@gmail.com>
2017-08-30 13:45:59 -0600

    Implemented tests and page redirection for admin functions

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     "test#{n}@example.com"
   end
 
-  factory :user do
+  factory :user, class: "User" do
     first_name 'Jon'
     last_name 'Snow'
     email "test@test.com"
@@ -15,6 +15,14 @@ FactoryGirl.define do
     first_name 'Admin'
     last_name 'User'
     email "admin@user.com"
+    password "asdfasdf"
+    password_confirmation "asdfasdf"
+  end
+
+  factory :non_authorized_user, class: "NonUser" do
+    first_name 'Non'
+    last_name 'User'
+    email "non@user.com"
     password "asdfasdf"
     password_confirmation "asdfasdf"
   end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -27,59 +27,6 @@ describe 'navigate' do
     end
   end
 
-  RSpec.describe Post, type: :model do
-    describe "Creation" do
-    before do
-      @post = FactoryGirl.create(:post)
-    end
-        
-  			visit new_post_path
-  		end
-
-	  	it 'has a new form that can be reached' do	  		
-	  		expect(page.status_code).to eq(200)
-	  	end
-
-	  	it 'can be created from new form page' do	  	  
-	  	  fill_in 'post[date]', with: Date.today
-	  	  fill_in 'post[rationale]', with: "Some rationale"
-	  	  click_on "Save"
-	  	  expect(page).to have_content("Some rationale")
-	  	end
-
-      it 'will have a user associated with it' do
-        fill_in 'post[date]', with: Date.today
-        fill_in 'post[rationale]', with: "User_Association"
-        click_on "Save"
-
-        expect(User.last.posts.last.rationale).to eq("User Association")
-      end
-  end
-
-  describe 'edit' do
-    before do
-      @post = FactoryGirl.create(:post)
-    end
-
-    it 'can be reached by clicking edit on index page' do
-      post = FactoryGirl.create(:post)
-      visit posts_path
-
-      click_link("edit_#{@post.id}")
-      expect(page.status_code).to eq(200)
-    end
-
-    it 'can be edited' do
-      visit edit_post_path(@post)
-
-      fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "Edited content"
-      click_on "Save"
-
-      expect(page).to have_content("Edited content")
-    end
-  end
-
   describe 'new' do
     it 'has a link from the homepage' do
       visit root_path
@@ -96,6 +43,60 @@ describe 'navigate' do
 
       click_link("delete_post_#{@post.id}_from_index")
       expect(page.status_code).to eq(200)
+    end
+  end
+
+  describe 'creation' do
+    before do
+      visit new_post_path
+    end
+
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
+
+      expect(page).to have_content("Some rationale")
+    end
+
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
+    end
+  end
+
+  describe 'edit' do
+    before do
+      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      login_as(@edit_user, :scope => :user)
+      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
+    end
+
+    it 'can be edited' do
+      visit edit_post_path(@edit_post)
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Edited content"
+      click_on "Save"
+
+      expect(page).to have_content("Edited content")
+    end
+
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authorized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
+
+      visit edit_post_path(@edit_post)
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
